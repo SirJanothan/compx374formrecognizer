5c4fbe5d2dc9989a5bd56390720aeb6c7f0b21af
SirJanothan <54874145+SirJanothan@users.noreply.github.com>
2021-09-20 13:16:53 +1200

    Add project files.

---
+++ compx374winform/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace compx374winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRecognize = new System.Windows.Forms.Button();
+            this.buttonTrainModel = new System.Windows.Forms.Button();
+            this.buttonTrainLabels = new System.Windows.Forms.Button();
+            this.buttonManage = new System.Windows.Forms.Button();
+            this.buttonAnalyzeForm = new System.Windows.Forms.Button();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.buttonSelectForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRecognize
+            // 
+            this.buttonRecognize.Location = new System.Drawing.Point(12, 12);
+            this.buttonRecognize.Name = "buttonRecognize";
+            this.buttonRecognize.Size = new System.Drawing.Size(214, 23);
+            this.buttonRecognize.TabIndex = 0;
+            this.buttonRecognize.Text = "Recognize Content";
+            this.buttonRecognize.UseVisualStyleBackColor = true;
+            this.buttonRecognize.Click += new System.EventHandler(this.ButtonRecognize_Click);
+            // 
+            // buttonTrainModel
+            // 
+            this.buttonTrainModel.Location = new System.Drawing.Point(12, 41);
+            this.buttonTrainModel.Name = "buttonTrainModel";
+            this.buttonTrainModel.Size = new System.Drawing.Size(214, 23);
+            this.buttonTrainModel.TabIndex = 1;
+            this.buttonTrainModel.Text = "Train Model";
+            this.buttonTrainModel.UseVisualStyleBackColor = true;
+            this.buttonTrainModel.Click += new System.EventHandler(this.ButtonTrainModel_Click);
+            // 
+            // buttonTrainLabels
+            // 
+            this.buttonTrainLabels.Location = new System.Drawing.Point(12, 70);
+            this.buttonTrainLabels.Name = "buttonTrainLabels";
+            this.buttonTrainLabels.Size = new System.Drawing.Size(214, 23);
+            this.buttonTrainLabels.TabIndex = 2;
+            this.buttonTrainLabels.Text = "Train Model With Labels";
+            this.buttonTrainLabels.UseVisualStyleBackColor = true;
+            this.buttonTrainLabels.Click += new System.EventHandler(this.ButtonTrainLabels_Click);
+            // 
+            // buttonManage
+            // 
+            this.buttonManage.Location = new System.Drawing.Point(12, 128);
+            this.buttonManage.Name = "buttonManage";
+            this.buttonManage.Size = new System.Drawing.Size(214, 23);
+            this.buttonManage.TabIndex = 3;
+            this.buttonManage.Text = "Manage Models";
+            this.buttonManage.UseVisualStyleBackColor = true;
+            this.buttonManage.Click += new System.EventHandler(this.ButtonManage_Click);
+            // 
+            // buttonAnalyzeForm
+            // 
+            this.buttonAnalyzeForm.Location = new System.Drawing.Point(12, 99);
+            this.buttonAnalyzeForm.Name = "buttonAnalyzeForm";
+            this.buttonAnalyzeForm.Size = new System.Drawing.Size(214, 23);
+            this.buttonAnalyzeForm.TabIndex = 4;
+            this.buttonAnalyzeForm.Text = "Analyze Form";
+            this.buttonAnalyzeForm.UseVisualStyleBackColor = true;
+            this.buttonAnalyzeForm.Click += new System.EventHandler(this.ButtonAnalyzeForm_Click);
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.Location = new System.Drawing.Point(12, 221);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(214, 23);
+            this.buttonSelectFolder.TabIndex = 5;
+            this.buttonSelectFolder.Text = "Select Folder";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.ButtonSelectFiles_Click);
+            // 
+            // buttonSelectForm
+            // 
+            this.buttonSelectForm.Location = new System.Drawing.Point(13, 251);
+            this.buttonSelectForm.Name = "buttonSelectForm";
+            this.buttonSelectForm.Size = new System.Drawing.Size(213, 23);
+            this.buttonSelectForm.TabIndex = 6;
+            this.buttonSelectForm.Text = "Select a Form";
+            this.buttonSelectForm.UseVisualStyleBackColor = true;
+            this.buttonSelectForm.Click += new System.EventHandler(this.ButtonSelectForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSelectForm);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.Controls.Add(this.buttonAnalyzeForm);
+            this.Controls.Add(this.buttonManage);
+            this.Controls.Add(this.buttonTrainLabels);
+            this.Controls.Add(this.buttonTrainModel);
+            this.Controls.Add(this.buttonRecognize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRecognize;
+        private System.Windows.Forms.Button buttonTrainModel;
+        private System.Windows.Forms.Button buttonTrainLabels;
+        private System.Windows.Forms.Button buttonManage;
+        private System.Windows.Forms.Button buttonAnalyzeForm;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.Button buttonSelectForm;
+    }
+}
+
---
+++ compx374winform/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Training;
+
+
+namespace compx374winform
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string endpoint = "https://jonathan-eddy-form-recognizer.cognitiveservices.azure.com/";
+        private static readonly string apiKey = "0a4823585ca04c5d991705bc318679b4";
+        private static readonly AzureKeyCredential credential = new AzureKeyCredential(apiKey);
+
+        private static string trainingDataUrl = "https://jonathaneddy374.blob.core.windows.net/universityapplication1?sp=rwdl&st=2021-09-13T05:29:51Z&se=2021-11-29T12:29:51Z&spr=https&sv=2020-08-04&sr=c&sig=i5h2ct8CjvQB%2FBe%2BvgA0zTGC5VnuH5ZpaarlJSSteHo%3D";
+        private static string formUrl = "https://jonathaneddy374.blob.core.windows.net/universityapplication1/IMG_0914.png?sp=rwd&st=2021-09-13T05:34:26Z&se=2021-11-29T12:34:26Z&spr=https&sv=2020-08-04&sr=b&sig=tkk4kC%2BUks%2Fx1haZX2v3GAXj%2FafTbZ%2BgrAzLjQRDl3g%3D";
+        private static string modelId = "f8a363a6-dbfa-4d5e-ad34-9edd04105d75";
+
+        private FormRecognizerClient recognizerClient;
+        private FormTrainingClient trainingClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            recognizerClient = AuthenticateClient();
+            trainingClient = AuthenticateTrainingClient();
+        }
+
+        private static FormRecognizerClient AuthenticateClient()
+        {
+            var credential = new AzureKeyCredential(apiKey);
+            var client = new FormRecognizerClient(new Uri(endpoint), credential);
+            return client;
+        }
+
+        static private FormTrainingClient AuthenticateTrainingClient()
+        {
+            var credential = new AzureKeyCredential(apiKey);
+            var client = new FormTrainingClient(new Uri(endpoint), credential);
+            return client;
+        }
+
+        private static async Task RecognizeContent(FormRecognizerClient recognizerClient)
+        {
+            var invoiceUri = formUrl;
+            FormPageCollection formPages = await recognizerClient
+                .StartRecognizeContentFromUri(new Uri(invoiceUri))
+                .WaitForCompletionAsync();
+            foreach (FormPage page in formPages)
+            {
+                Console.WriteLine($"Form Page {page.PageNumber} has {page.Lines.Count} lines.");
+
+                for (int i = 0; i < page.Lines.Count; i++)
+                {
+                    FormLine line = page.Lines[i];
+                    Console.WriteLine($"    Line {i} has {line.Words.Count} word{(line.Words.Count > 1 ? "s" : "")}, and text: '{line.Text}'.");
+                }
+
+                for (int i = 0; i < page.Tables.Count; i++)
+                {
+                    FormTable table = page.Tables[i];
+                    Console.WriteLine($"Table {i} has {table.RowCount} rows and {table.ColumnCount} columns.");
+                    foreach (FormTableCell cell in table.Cells)
+                    {
+                        Console.WriteLine($"    Cell ({cell.RowIndex}, {cell.ColumnIndex}) contains text: '{cell.Text}'.");
+                    }
+                }
+            }
+        }
+
+        private static async Task<String> TrainModel(FormTrainingClient trainingClient, string trainingDataUrl)
+        {
+            CustomFormModel model = await trainingClient
+            .StartTrainingAsync(new Uri(trainingDataUrl), useTrainingLabels: false)
+            .WaitForCompletionAsync();
+
+            Console.WriteLine($"Custom Model Info:");
+            Console.WriteLine($"    Model Id: {model.ModelId}");
+            Console.WriteLine($"    Model Status: {model.Status}");
+            Console.WriteLine($"    Training model started on: {model.TrainingStartedOn}");
+            Console.WriteLine($"    Training model completed on: {model.TrainingCompletedOn}");
+            foreach (CustomFormSubmodel submodel in model.Submodels)
+            {
+                Console.WriteLine($"Submodel Form Type: {submodel.FormType}");
+                foreach (CustomFormModelField field in submodel.Fields.Values)
+                {
+                    Console.Write($"    FieldName: {field.Name}");
+                    if (field.Label != null)
+                    {
+                        Console.Write($", FieldLabel: {field.Label}");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            return model.ModelId;
+        }
+
+        private static async Task<String> TrainModelWithLabels(FormTrainingClient trainingClient, string trainingDataUrl)
+        {
+            CustomFormModel model = await trainingClient
+            .StartTrainingAsync(new Uri(trainingDataUrl), useTrainingLabels: true)
+            .WaitForCompletionAsync();
+            Console.WriteLine($"Custom Model Info:");
+            Console.WriteLine($"    Model Id: {model.ModelId}");
+            Console.WriteLine($"    Model Status: {model.Status}");
+            Console.WriteLine($"    Training model started on: {model.TrainingStartedOn}");
+            Console.WriteLine($"    Training model completed on: {model.TrainingCompletedOn}");
+            foreach (CustomFormSubmodel submodel in model.Submodels)
+            {
+                Console.WriteLine($"Submodel Form Type: {submodel.FormType}");
+                foreach (CustomFormModelField field in submodel.Fields.Values)
+                {
+                    Console.Write($"    FieldName: {field.Name}");
+                    if (field.Label != null)
+                    {
+                        Console.Write($", FieldLabel: {field.Label}");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            return model.ModelId;
+        }
+
+        // Analyze PDF form data
+        private static async Task AnalyzePdfForm(FormRecognizerClient recognizerClient, String modelId, string formUrl)
+        {
+            RecognizedFormCollection forms = await recognizerClient
+            .StartRecognizeCustomFormsFromUri(modelId, new Uri(formUrl))
+            .WaitForCompletionAsync();
+            foreach (RecognizedForm form in forms)
+            {
+                Console.WriteLine($"Form of type: {form.FormType}");
+                foreach (FormField field in form.Fields.Values)
+                {
+                    Console.WriteLine($"Field '{field.Name}: ");
+
+                    if (field.LabelData != null)
+                    {
+                        Console.WriteLine($"    Label: '{field.LabelData.Text}");
+                    }
+
+                    Console.WriteLine($"    Value: '{field.ValueData.Text}");
+                    Console.WriteLine($"    Confidence: '{field.Confidence}");
+                }
+                Console.WriteLine("Table data:");
+                foreach (FormPage page in form.Pages)
+                {
+                    for (int i = 0; i < page.Tables.Count; i++)
+                    {
+                        FormTable table = page.Tables[i];
+                        Console.WriteLine($"Table {i} has {table.RowCount} rows and {table.ColumnCount} columns.");
+                        foreach (FormTableCell cell in table.Cells)
+                        {
+                            Console.WriteLine($"    Cell ({cell.RowIndex}, {cell.ColumnIndex}) contains {(cell.IsHeader ? "header" : "text")}: '{cell.Text}'");
+                        }
+                    }
+                }
+            }
+        }
+        private static async Task AnalyzePdfForm(FormRecognizerClient recognizerClient, String modelId, FileStream filestream)
+        {
+            RecognizedFormCollection forms = await recognizerClient
+            .StartRecognizeCustomForms(modelId, filestream)
+            .WaitForCompletionAsync();
+            foreach (RecognizedForm form in forms)
+            {
+                Console.WriteLine($"Form of type: {form.FormType}");
+                foreach (FormField field in form.Fields.Values)
+                {
+                    Console.WriteLine($"Field '{field.Name}: ");
+
+                    if (field.LabelData != null)
+                    {
+                        Console.WriteLine($"    Label: '{field.LabelData.Text}");
+                    }
+
+                    Console.WriteLine($"    Value: '{field.ValueData.Text}");
+                    Console.WriteLine($"    Confidence: '{field.Confidence}");
+                }
+                Console.WriteLine("Table data:");
+                foreach (FormPage page in form.Pages)
+                {
+                    for (int i = 0; i < page.Tables.Count; i++)
+                    {
+                        FormTable table = page.Tables[i];
+                        Console.WriteLine($"Table {i} has {table.RowCount} rows and {table.ColumnCount} columns.");
+                        foreach (FormTableCell cell in table.Cells)
+                        {
+                            Console.WriteLine($"    Cell ({cell.RowIndex}, {cell.ColumnIndex}) contains {(cell.IsHeader ? "header" : "text")}: '{cell.Text}'");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static async Task ManageModels(FormTrainingClient trainingClient, string trainingFileUrl)
+        {
+            // Check number of models in the FormRecognizer account, 
+            // and the maximum number of models that can be stored.
+            AccountProperties accountProperties = trainingClient.GetAccountProperties();
+            Console.WriteLine($"Account has {accountProperties.CustomModelCount} models.");
+            Console.WriteLine($"It can have at most {accountProperties.CustomModelLimit} models.");
+            Pageable<CustomFormModelInfo> models = trainingClient.GetCustomModels();
+
+            foreach (CustomFormModelInfo modelInfo in models)
+            {
+                Console.WriteLine($"Custom Model Info:");
+                Console.WriteLine($"    Model Id: {modelInfo.ModelId}");
+                Console.WriteLine($"    Model Status: {modelInfo.Status}");
+                Console.WriteLine($"    Training model started on: {modelInfo.TrainingStartedOn}");
+                Console.WriteLine($"    Training model completed on: {modelInfo.TrainingCompletedOn}");
+            }
+
+
+            // Create a new model to store in the account
+            CustomFormModel model = await trainingClient.StartTrainingAsync(
+                new Uri(trainingFileUrl), useTrainingLabels: false).WaitForCompletionAsync();
+
+            // Get the model that was just created
+            CustomFormModel modelCopy = trainingClient.GetCustomModel(model.ModelId);
+
+            Console.WriteLine($"Custom Model {modelCopy.ModelId} recognizes the following form types:");
+
+            foreach (CustomFormSubmodel submodel in modelCopy.Submodels)
+            {
+                Console.WriteLine($"Submodel Form Type: {submodel.FormType}");
+                foreach (CustomFormModelField field in submodel.Fields.Values)
+                {
+                    Console.Write($"    FieldName: {field.Name}");
+                    if (field.Label != null)
+                    {
+                        Console.Write($", FieldLabel: {field.Label}");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            // Delete the model from the account.
+            trainingClient.DeleteModel(model.ModelId);
+        }
+
+        private void ButtonRecognize_Click(object sender, EventArgs e)
+        {
+            var recognizeContent = RecognizeContent(recognizerClient);
+            //Task.WaitAll(recognizeContent);
+        }
+
+        private void ButtonTrainModel_Click(object sender, EventArgs e)
+        {
+            var trainModel = TrainModel(trainingClient, trainingDataUrl);
+            //Task.WaitAll(trainModel);
+        }
+
+        private void ButtonTrainLabels_Click(object sender, EventArgs e)
+        {
+            //var trainModelWithLabels = TrainModelWithLabels(trainingClient, trainingDataUrl);
+            //Task.WaitAll(trainModel);
+        }
+
+        private void ButtonAnalyzeForm_Click(object sender, EventArgs e)
+        {
+            var analyzeForm = AnalyzePdfForm(recognizerClient, modelId, formUrl);
+            //Task.WaitAll(analyzeForm);
+        }
+
+        private void ButtonManage_Click(object sender, EventArgs e)
+        {
+            var manageModels = ManageModels(trainingClient, trainingDataUrl);
+            //Task.WaitAll(manageModels);
+        }
+
+        private void ButtonSelectFiles_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("OK");
+            }
+        }
+
+        private void ButtonSelectForm_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = File.OpenRead(fileDialog.FileName);
+                var analyzeForm = AnalyzePdfForm(recognizerClient, modelId, fs);
+            }
+        }
+    }
+}
